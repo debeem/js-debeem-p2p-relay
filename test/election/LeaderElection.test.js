3d9913a6c6f1b124982a51441a7e8b3dcdfaad51
deframework <deframework@outlook.com>
2024-11-01 00:49:47 +0800

    "version": "1.0.31", 1, added node LeaderElection.js 2, use winston for log print instead of console.log to print log

--- test/election/LeaderElection.test.js
+++ test/election/LeaderElection.test.js
@@ -2,6 +2,7 @@ import assert from "assert";
 import { ProcessUtil, TimerUtil } from "debeem-utils";
 import { PeerUtil, RelayOptionsBuilder, RelayService } from "../../src/index.js";
 import { bootstrappers } from "../../examples/bootstrappers.js";
+import _ from "lodash";
 
 let fakeRelayServices = [];
 
@@ -23,61 +24,75 @@ describe( 'LeaderElection', function ()
 
 	describe( 'methods', function ()
 	{
-		it( '#handlePeerEvent', async ( done ) =>
+		it( '#handlePeerEvent', () =>
 		{
 			this.timeout( 80 * 1000 );
-
-			setTimeout( () =>
+			return new Promise( async ( resolve, reject ) =>
 			{
-				done();
+				try
+				{
+					const ports = [ 61001, 61002, 61003 ];
+					const peerIdFiles = [
+						`./peers/.relay1.peerId`,
+						`./peers/.relay2.peerId`,
+						`./peers/.relay3.peerId`,
+					];
 
-			}, 60 * 1000 );
+					for ( let i = 0; i < ports.length; i ++ )
+					{
+						const port = ports[ i ];
+						const announceAddresses = PeerUtil.getAnnounceAddresses();
+						const peerIdFilename = peerIdFiles[ i ];
+						const swarmKeyFilename = ProcessUtil.getParamStringValue( `swarm_key`, undefined );
+						const createRelayOptions = RelayOptionsBuilder.builder()
+							.setPeerIdFilename( peerIdFilename )
+							.setSwarmKeyFilename( swarmKeyFilename )
+							.setPort( port )
+							.setAnnounceAddresses( announceAddresses )
+							.setBootstrapperAddresses( bootstrappers )
+							.setPubsubPeerDiscoveryTopics( [] )
+							.build();
+						console.log( `////////// ////////// ////////// ////////// ////////// //////////` );
+						console.log( `will createRelay with options: `, createRelayOptions );
+						const relayService = new RelayService();
+						const relayNode = await relayService.createRelay( createRelayOptions );
 
-			const ports = [ 61001, 61002, 61003 ];
-			const peerIdFiles = [
-				`./peers/.relay1.peerId`,
-				`./peers/.relay2.peerId`,
-				`./peers/.relay3.peerId`,
-			];
+						//	...
+						fakeRelayServices.push( relayService );
 
-			for ( let i = 0; i < ports.length; i ++ )
-			{
-				const port = ports[ i ];
-				const announceAddresses = PeerUtil.getAnnounceAddresses();
-				const peerIdFilename = peerIdFiles[ i ];
-				const swarmKeyFilename = ProcessUtil.getParamStringValue( `swarm_key`, undefined );
-				const createRelayOptions = RelayOptionsBuilder.builder()
-					.setPeerIdFilename( peerIdFilename )
-					.setSwarmKeyFilename( swarmKeyFilename )
-					.setPort( port )
-					.setAnnounceAddresses( announceAddresses )
-					.setBootstrapperAddresses( bootstrappers )
-					.setPubsubPeerDiscoveryTopics( [] )
-					.build();
-				console.log( `////////// ////////// ////////// ////////// ////////// //////////` );
-				console.log( `will createRelay with options: `, createRelayOptions );
-				const relayService = new RelayService();
-				const relayNode = await relayService.createRelay( createRelayOptions );
+						//	...
+						await TimerUtil.waitForDelay( 1000 );
+					}
 
-				//	...
-				fakeRelayServices.push( relayService );
+					assert.strictEqual( fakeRelayServices.length, ports.length );
+					setTimeout( async () =>
+					{
+						//	make the leader node offline
+						//await fakeRelayServices[ 0 ].stop();
 
-				//	...
-				await TimerUtil.waitForDelay( 1000 );
-			}
+						console.log( process.env );
+						assert.strictEqual(
+							`true` === process.env.P2P_RELAY_IS_LEADER || `false` === process.env.P2P_RELAY_IS_LEADER,
+							true
+						);
+						assert.strictEqual(
+							_.isString( process.env.P2P_RELAY_LEADER_PEER_ID ) && ! _.isEmpty( process.env.P2P_RELAY_LEADER_PEER_ID ),
+							true
+						);
 
-			assert.strictEqual( fakeRelayServices.length, ports.length );
-			setTimeout( async () =>
-			{
-				//	make the leader node offline
-				await fakeRelayServices[ 0 ].stop();
+						//	...
+						resolve();
 
-			}, 10 * 1000 );
+					}, 15 * 1000 );
 
-			await TimerUtil.waitForDelay( 80 * 1000 );
-
-			assert.strictEqual( fakeRelayServices.length, ports.length );
-
+					//await TimerUtil.waitForDelay( 30 * 1000 );
+					assert.strictEqual( fakeRelayServices.length, ports.length );
+				}
+				catch ( err )
+				{
+					reject( err );
+				}
+			});
 		} );
 	} );
 } );
